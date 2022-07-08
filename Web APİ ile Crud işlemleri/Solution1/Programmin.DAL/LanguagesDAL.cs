9e6e715463ce9b906497c3bd45754b307d39502a
Samettt47 <sametbyrktr47@gmail.com>
2022-07-08 21:30:35 +0300

    projelerim

---
+++ Web APİ ile Crud işlemleri/Solution1/Programmin.DAL/LanguagesDAL.cs
@@ -0,0 +1,52 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmin.DAL
+{
+
+    public class LanguagesDAL
+    {
+        ProgrammingDbEntities db = new ProgrammingDbEntities();
+
+        public IEnumerable<Languages> GetAllLanguages()
+        {
+            return db.Languages;
+
+        }
+        public Languages GetLanguagesByID(int id)
+        {
+            return db.Languages.Find(id);
+
+        }
+        
+        public Languages CreateLangueage(Languages language)
+        {
+            db.Languages.Add(language);
+            db.SaveChanges();
+            return language;
+        }
+        public Languages Updatelanguages(int id , Languages language)
+        {
+            db.Entry(language).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return language;
+
+
+        }
+        public void DeleteLAnguage(int id)
+        {
+            db.Languages.Remove(db.Languages.Find(id));
+            db.SaveChanges();
+
+        }
+        
+
+        }
+
+
+    }
+
+
---
+++ Web APİ ile Crud işlemleri/Solution1/Programming.API/Controllers/LanguagesController.cs
@@ -0,0 +1,49 @@
+﻿using Programmin.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace Programming.API.Controllers
+{
+   
+    public class LanguagesController : ApiController
+    {
+
+        public IEnumerable<Languages> Get() {
+            LanguagesDAL languagesDAL = new LanguagesDAL();
+
+            return languagesDAL.GetAllLanguages();
+
+        }
+        public Languages Get(int id)
+        {
+            LanguagesDAL languagesDAL = new LanguagesDAL();
+
+            return languagesDAL.GetLanguagesByID(id);
+        }
+        public Languages Post(Languages language)
+        {
+            LanguagesDAL languagesDAL = new LanguagesDAL();
+
+            return languagesDAL.CreateLangueage(language);
+        }
+        public Languages Put(int id , Languages language)
+        {
+            LanguagesDAL languagesDAL = new LanguagesDAL();
+
+            return languagesDAL.Updatelanguages(id, language);
+
+        }
+        public void Delete(int id)
+        {
+            LanguagesDAL languagesDAL = new LanguagesDAL();
+
+            languagesDAL.DeleteLAnguage(id);
+
+        }
+    }
+}
